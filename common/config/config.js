a90310ff4ed689bfc7476be71a073beee04d8465
Mohammad Sakr <mohammad_sakr_89@yahoo.com>
2020-06-17 15:55:55 +0200

    Config env vars

--- common/config/config.js
+++ common/config/config.js
@@ -5,21 +5,7 @@ const config = {
   db: {
     mongoURI: process.env.MONGO_URI
   },
-  jwt: {
-    key: process.env.JWT_SECRET,
-    expire: process.env.JWT_EXPIRE
-  },
-  baseUrl: process.env.BASE_API_URL,
-  salt: process.env.BCRYPT_SALT_ROUNDS,
-  redis: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    rqmsNamespace: process.env.RQMS_NAME_SPACE,
-    notificationQueue: process.env.REDIS_NOTIFICATION_QUEUE
-  },
-  events: {
-    notifications: process.env.NOTIFICATION_EVENT
-  }
+  baseUrl: process.env.BASE_API_URL
 };
 
 module.exports = config;
